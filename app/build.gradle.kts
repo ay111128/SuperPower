5c1209817f9bca1aaeb18ac2221448ae051e7007
Ubuntu <ubuntu@localhost.localdomain>
2026-09-15 22:01:08 +0800

    chore: bump version 0.2.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.paperbox.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.2.10"
+        versionCode = 14
+        versionName = "0.2.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
