71f7f4591d020ac7ba383b02139d23458b14692e
Ubuntu <ubuntu@localhost.localdomain>
2026-09-20 18:24:35 +0800

    fix: 补上 fillMaxHeight import

--- app/src/main/java/com/paperbox/app/ui/quote/QuoteResultPage.kt
+++ app/src/main/java/com/paperbox/app/ui/quote/QuoteResultPage.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.fillMaxWidth
