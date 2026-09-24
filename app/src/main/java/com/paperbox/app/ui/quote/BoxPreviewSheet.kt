692ca5b48d53a5431311a9f3309833311db0b8b8
Ubuntu <ubuntu@localhost.localdomain>
2026-09-24 10:22:27 +0800

    fix: BoxPreviewSheet补WindowInsets.ime import(CI编译失败)

--- app/src/main/java/com/paperbox/app/ui/quote/BoxPreviewSheet.kt
+++ app/src/main/java/com/paperbox/app/ui/quote/BoxPreviewSheet.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
